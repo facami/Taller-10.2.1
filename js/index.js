758485fa235146b57e6c557f944b92389b9071c5
FaCami <camilafarroyo16842@hotmail.com>
2024-10-06 18:43:05 -0300

    Update index.js

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('buttonText').addEventListener('click', function() {
+    const inputValue = document.getElementById('inputText').value;
+    localStorage.setItem('userInput', inputValue);
+});
